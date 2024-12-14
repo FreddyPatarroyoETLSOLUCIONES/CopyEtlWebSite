d30ac51697192887169240d41ed4fef502e23de8
JorgeAndresETL <jorge.salas@etlsoluciones.com>
2024-12-14 01:53:33 -0500

    Creación loading screen en componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NabvarComponent } from './nabvar/nabvar.component'; 
+import { PrincipalComponent } from './principal/principal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NabvarComponent],
+  imports: [
+    NabvarComponent,
+    PrincipalComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
