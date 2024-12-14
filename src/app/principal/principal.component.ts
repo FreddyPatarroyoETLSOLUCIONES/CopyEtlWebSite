837876cc907aabcd11dc444e997b3998000e0907
Freddy Patarroyo <freddy.patarroyo@etlsoluciones.com>
2024-12-14 10:00:01 -0500

    creando componente welcome

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @Component({
   selector: 'app-principal',
-  imports: [],
+  imports: [WelcomeComponent],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
