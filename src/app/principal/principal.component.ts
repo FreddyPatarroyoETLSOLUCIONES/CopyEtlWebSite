62a388be6fe1b1c27787a1d68bd353f9dedb886f
JorgeAndresETL <jorge.salas@etlsoluciones.com>
2024-12-15 02:47:07 -0500

    Creación componente carousel

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @Component({
   selector: 'app-principal',
-  imports: [WelcomeComponent],
+  imports: [
+    WelcomeComponent,
+    CarouselComponent,
+  ],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
 })
