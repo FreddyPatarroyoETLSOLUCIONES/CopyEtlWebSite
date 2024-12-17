d434080a255cc3c5527676829832015421d15380
Freddy Patarroyo <freddy.patarroyo@etlsoluciones.com>
2024-12-17 11:09:24 -0500

    inicio componente productos

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { ProductsComponent } from './products/products.component';
 
 @Component({
   selector: 'app-principal',
   imports: [
     WelcomeComponent,
     CarouselComponent,
+    ProductsComponent
   ],
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css'
