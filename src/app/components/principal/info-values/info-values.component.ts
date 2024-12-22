b811e21e0dd28077fc63d083eff78f0ef5b9abe8
Freddy Patarroyo <freddy.patarroyo@etlsoluciones.com>
2024-12-22 16:18:34 -0500

    ajustes del modelo de texto

--- src/app/components/principal/info-values/info-values.component.ts
+++ src/app/components/principal/info-values/info-values.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
 import { GalleriaModule } from 'primeng/galleria';
+import { Text } from '../../../models/texts';
 
 @Component({
   selector: 'app-info-values',
@@ -10,27 +11,10 @@ import { GalleriaModule } from 'primeng/galleria';
   host: { ngSkipHydration: 'true' },
  
 })
-export class InfoValuesComponent {
-  services = [
-    {
-      title: 'Atención Inmediata',
-      description: 'Somos oportunos en cada requerimiento solicitado por nuestros clientes.',
-      image: './assets/wallpapers/atencioninmediata.jpg'
-    },
-    {
-      title: 'Estrategia',
-      description: 'No solo buscamos estrategias y herramientas sino soluciones que impacten.',
-      image: './assets/wallpapers/estrategia.jpg'
-    },
-    {
-      title: 'Optimización',
-      description: 'Continuamente queremos mantener nuestras aplicaciones de manera óptima y efectiva.',
-      image: './assets/wallpapers/optimizacion.jpg'
-    },
-    {
-      title: 'Innovación',
-      description: 'Queremos siempre estar a la vanguardia en la utilización del desarrollo y las tecnologías.',
-      image: './assets/wallpapers/innovacion_2.jpg'
-    }
-  ];
+export class InfoValuesComponent implements OnInit{
+  services!:any[];
+
+  ngOnInit(): void {
+    this.services = Text.Principal.InfoValues.Services;
+  }
 }
