ebc61f6d56d2d7120c094aa211d095572a676760
JorgeAndresETL <jorge.salas@etlsoluciones.com>
2025-04-05 21:25:56 -0500

    Logica Form y Ajustes generales

--- src/app/items/form/form.component.ts
+++ src/app/items/form/form.component.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  formData = {
+    name: '',
+    email: '',
+    phone: '',
+    project: ''
+  };
+
+  submitted = false;
+
+  onSubmit(form: NgForm) {
+    this.submitted = true;
+
+    if (form.invalid) {
+      return;
+    }
+
+    alert('¡Formulario enviado correctamente!');
+    form.resetForm();
+    this.submitted = false;
+  }
 
 }
